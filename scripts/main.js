7b9b93f098112e71237de4992996fa380c2cc7e3
alexeyb189 <alexeybmusic@gmail.com>
2025-03-15 23:50:20 +0700

    qwq

--- scripts/main.js
+++
@@ -1,7 +0,0 @@
-const text = document.querySelector(".circle-text");
-text.innerHTML = text.innerText
-	.split("")
-	.map(
-		(char, i) => `<span style="transform:rotate(${i * 12.3}deg)">${char}</span>`
-	)
-	.join("");
